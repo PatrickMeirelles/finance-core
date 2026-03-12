3599e021686504e4c42149bd0198af332aa717a4
Patrick Meirelles <meirelles.dev@gmail.com>
2026-03-11 22:51:07 -0300

    [Feat] setup response

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
-import { GlobalResponseInterceptor } from './common/interceptors/response-interceptor.filter';
+import { GlobalResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
